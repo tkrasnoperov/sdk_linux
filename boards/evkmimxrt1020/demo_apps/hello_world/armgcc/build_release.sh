3bdfe5159d4cfbfca257d6726124effba461327a
Tim Krasnoperov <tkrasnoperov@Tims-MacBook-Pro.local>
2020-11-14 16:10:14 -0800

    f

--- boards/evkmimxrt1020/demo_apps/hello_world/armgcc/build_release.sh
+++ boards/evkmimxrt1020/demo_apps/hello_world/armgcc/build_release.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+export ARMGCC_DIR=/opt/gcc-arm-none-eabi-9-2019-q4-major
 cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=release  .
 make -j 2>&1 | tee build_log.txt
